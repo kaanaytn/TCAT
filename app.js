32af1148e265be37b4ec966ed83346fbd996fe98
kaan <kaanayten@hotmail.com>
2022-11-28 10:49:44 +0300

    Photos page updated

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
 
+// CONNECT DB
+mongoose.connect('mongodb://localhost/tcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
 // MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+// ROUTES
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
+});
+
+app.get('/photos/:id', async (req, res) => {
+  // console.log(req.params.id);
+  // res.render('about');
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
